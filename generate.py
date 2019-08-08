7f9e851cb2783a5de56464d85e382ff8324fdfc0
neubau <minijatura@fastmail.com>
2019-08-08 15:21:57 +0200

    All so far

--- generate.py
+++ generate.py
@@ -3,23 +3,26 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import os
 import random
 
-vocab = {}
-with open("vocab.txt") as f:
-    lines = f.readlines()
-    random.shuffle(lines)
-    for line in lines:
-        try:
-            lemma, definition = re.split("\s\s+", line, maxsplit=1)
-            vocab[lemma] = definition
-            if len(vocab) > 100:
-                break
-        except:
-            print("Problem: " + line)
-            
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 slider_template = env.get_template("slider.html")
-with open('docs/vocab.html', 'w') as vocab_file:
-    print(slider_template.render(vocab=vocab), file=vocab_file)
+
+with open("vocab.txt") as f:
+    lines = f.readlines()
+    random.shuffle(lines)
+    vocab = {}
+    file_id = 1
+    for line in lines:
+        try:
+            lemma, definition = re.split("\s\s+", line, maxsplit=1)
+            vocab[lemma] = definition
+            if len(vocab) == 100:
+                with open(f'docs/{file_id}.html', 'w') as vocab_file:
+                    print(slider_template.render(vocab=vocab), file=vocab_file)                 
+                file_id = file_id + 1
+                vocab = {}
+        except:
+            print("Problem: " + line)
+            
